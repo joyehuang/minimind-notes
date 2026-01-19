75d3f25c88d95e19c92e0806668d2aae3049e0bc
De-Shiou Huang <huangdeshiou@gmail.com>
2026-01-19 23:05:36 +0800

    fix(docs): include svg sprite component

---
+++ .vitepress/theme/components/SvgSprite.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="svg-sprite" aria-hidden="true" v-html="sprite"></div>
+</template>
+
+<script setup lang="ts">
+import sprite from '../assets/icons-sprite.svg?raw'
+</script>
+
+<style scoped>
+.svg-sprite {
+  position: absolute;
+  width: 0;
+  height: 0;
+  overflow: hidden;
+}
+</style>
